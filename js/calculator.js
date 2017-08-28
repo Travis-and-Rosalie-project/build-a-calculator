d61e70b6090d84a3a8269663de5ff95c3ad9096a
rosaliework <rosalie.gomez12@gmail.com>
2017-08-28 14:03:58 -0500

    start js

--- js/calculator.js
+++ js/calculator.js
@@ -1,36 +1,18 @@
 "use strict";
+console.log("hey");
 
-
-
-// document.ready(function(){
-//     var testNumLength = function(numbers) {
-//         if (numbers.length > 9) {
-//             totaldiv.text(number.substr(numbers.length-9,9));
-//             if (numbers.length > 15) {
-//                 numbers = "";
-//                 totaldiv.text("0");
-//             }
-//         }
-//     };
-//     var number = "";
-//     var newnumber = "";
-//     var operator = ""
-//
-// });
-//     //Add your var's here!
-//
-// });
 
 
     var total = document.getElementById("total");
     var numbers = document.getElementsByTagName('button');
     var len = numbers.length;
 
-    total.innerHTML = ;
+
 
     for(var i = 0; i < len; i++){
-    elem[i].addEventListener("click", function(){
-
-    numbers[i].addEventListener('click', numberFunction, false);
-    })}
-// }
+        numbers[i].addEventListener("click", function(){
+            console.log('hey!')
+             // alert(this);
+            console.log(this.innerHTML);
+        })
+    }
