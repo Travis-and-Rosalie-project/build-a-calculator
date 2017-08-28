e05f1d67f91f443aa6b8c110a8e13b7a206b80e2
rosaliework <rosalie.gomez12@gmail.com>
2017-08-28 13:39:43 -0500

    start js dom/bom stuff

--- js/calculator.js
+++ js/calculator.js
@@ -1,10 +1,36 @@
 "use strict";
 
-window.onload = function() {
-    var numberBox = document.getElementById("number-box");
-    addEventListener("click", function() {
-        var num = this.value;
-        var output = numberBox.innerHTML += num;
 
-    });
-};
+
+// document.ready(function(){
+//     var testNumLength = function(numbers) {
+//         if (numbers.length > 9) {
+//             totaldiv.text(number.substr(numbers.length-9,9));
+//             if (numbers.length > 15) {
+//                 numbers = "";
+//                 totaldiv.text("0");
+//             }
+//         }
+//     };
+//     var number = "";
+//     var newnumber = "";
+//     var operator = ""
+//
+// });
+//     //Add your var's here!
+//
+// });
+
+
+    var total = document.getElementById("total");
+    var numbers = document.getElementsByTagName('button');
+    var len = numbers.length;
+
+    total.innerHTML = ;
+
+    for(var i = 0; i < len; i++){
+    elem[i].addEventListener("click", function(){
+
+    numbers[i].addEventListener('click', numberFunction, false);
+    })}
+// }
